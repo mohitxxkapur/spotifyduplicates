b370e9064c4ee0613476e56e6ccc0e5877888ac5
Mohit Kapur <mohit19k9@gmail.com>
2021-09-13 19:33:12 -0400

    remove break from rng

--- RemoveDupes.py
+++ RemoveDupes.py
@@ -6,9 +6,6 @@ def rng(lim):
     while(lim < 0): 
         if lim < 0:
             myError = ValueError('Lim should be positive')
-            raise myError
-        else: 
-            break
 
     randval = random.randint(0,lim)
     return randval
